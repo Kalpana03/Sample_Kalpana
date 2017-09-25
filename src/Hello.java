7048261535961d9a75db0660414614579f355c9d
Kalpana03 <31638383+Kalpana03@users.noreply.github.com>
2017-09-24 23:08:41 -0400

    New_branch

--- src/Hello.java
+++ src/Hello.java
@@ -3,13 +3,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Learning about Github");
-<<<<<<< HEAD
 		System.out.println("We are in New Branch");
-=======
 
 		System.out.println("Hello Kalpana. We are in Majeti Branch");
 		System.out.println("Hello Kalpana we are in gitbashBranch");
->>>>>>> refs/remotes/origin/master
 	}
 
 }
